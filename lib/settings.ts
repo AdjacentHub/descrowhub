9f1238a0c51cacecb38fff7ca4962b537724967e
AdjacentHub <119417676+AdjacentHub@users.noreply.github.com>
2022-11-29 18:59:15 +0100

    Add files via upload

---
+++ lib/settings.ts
@@ -0,0 +1,9 @@
+export const settings = {
+    localStorageKey: 'signed_token',
+    SIGN_MESSAGE: 'I accept the AdjacentEscrowHub Terms of Service: https://descrowhub.com/tos',
+    ipfsGateways: [
+        "https://cloudflare-ipfs.com/ipfs/%s/%s",
+        "https://%s.ipfs.dweb.link/%s",
+        "https://ipfs.io/ipfs/%s/%s"
+    ]
+}
