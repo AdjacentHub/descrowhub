ca47f05b7569a14144dfc67371294785b6e40896
AdjacentHub <119417676+AdjacentHub@users.noreply.github.com>
2022-11-30 13:44:41 +0100

    Update settings.ts

--- lib/settings.ts
+++ lib/settings.ts
@@ -2,8 +2,6 @@ export const settings = {
     localStorageKey: 'signed_token',
     SIGN_MESSAGE: 'I accept the AdjacentEscrowHub Terms of Service: https://descrowhub.com/tos',
     ipfsGateways: [
-        "https://cloudflare-ipfs.com/ipfs/%s/%s",
-        "https://%s.ipfs.dweb.link/%s",
         "https://w3s.link/ipfs/%s/%s"
     ]
 }
